3d1b07c3490628b38870c6bb14e32e6d705744a9
Erica Chambers <53728223+ebc1201@users.noreply.github.com>
2023-03-20 15:51:45 -0500

    Deleted extra pages and added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./Weather.css";
 import Weather from "./Weather";
+import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Weather />
+      <footer>
+        This project was coded by Erica Chambers and is
+        <a href="https://github.com/ebc1201/weather-app-react" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
